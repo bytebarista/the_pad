67b60949540ea9e5517c6cda05feb038059e1338
Sebastian Roll <sebastian.roll@webstep.no>
2019-07-09 11:03:29 +0200

    Wip

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+import sys
 from setuptools import setup, find_packages
 import sdist_upip
 
-setup(
-    name="micropython-the-pad",
-    version="0.1",
-    packages=find_packages(),
-    cmdclass={'sdist': sdist_upip.sdist}
-)
+setup(name='micropython-the-pad',
+      #use_scm_version=True,
+      #setup_requires=['setuptools_scm'],
+      description='Modules and demos for the pad',
+      long_description="Modules and demos for the pad",
+      url='https://github.com/bytebarista/the_pad',
+      author='ByteBarista',
+      license='MIT',
+      cmdclass={'sdist': sdist_upip.sdist},
+      version="1.0.6",
+      py_modules=[]
+      #packages=find_packages(),
+      #py_modules=['the_pad/boot.py'])
+      )
