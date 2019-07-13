292867d62ccf0e7417edf78b637b13d12b948deb
Tudor Avram <t.avram@eyeo.com>
2019-07-13 12:51:34 +0200

    added buttons support

--- the_pad/controller.py
+++ the_pad/controller.py
@@ -1,7 +1,10 @@
 """
 High level access to buttons, thumb stick and .
 """
-from machine import Pin, ADC
+from machine import Pin, ADC, I2C
+
+from the_pad import pins
+import mcpnew
 
 
 class ThumbStick(object):
@@ -50,4 +53,104 @@ class ThumbStick(object):
         return (access.read() - 2047.5) / 2047.5
 
 
-thumbstick = ThumbStick()
+class Buttons:
+    """Controller class for the 4 buttons on the right hand side.
+
+    Parameters
+    ----------
+    io_handler: mcpnew.MCP
+        Handles signals from the buttons. If `None`, it will automatically be
+        assigned to the default.
+
+    """
+
+    BUTTON_LEFT = 5
+    BUTTON_RIGHT = 6
+    BUTTON_UP = 7
+    BUTTON_DOWN = 4
+    PINS = [BUTTON_LEFT, BUTTON_RIGHT, BUTTON_UP, BUTTON_DOWN]
+
+    def __init__(self, io_handler=None):
+        self.io_handler = io_handler
+        self._setup()
+
+    def _setup(self):
+        if self.io_handler is None:
+            i2c = I2C(scl=Pin(pins.I2C_SCL), sda=Pin(pins.I2C_SDA))
+            self.io_handler = mcpnew.MCP23017(i2c)
+
+        for pin in self.PINS:
+            self.io_handler.setup(pin, mcpnew.IN)
+            self.io_handler.pullup(pin, True)
+
+    @property
+    def up(self):
+        """Check if the up button is pressed.
+
+        Returns
+        -------
+        bool
+            Whether the button is pressed or not.
+
+        """
+        return not self.io_handler.input(self.BUTTON_UP)
+
+    @property
+    def down(self):
+        """Check if the down button is pressed.
+
+        Returns
+        -------
+        bool
+            Whether the button is pressed or not.
+
+        """
+        return not self.io_handler.input(self.BUTTON_DOWN)
+
+    @property
+    def right(self):
+        """Check if the right button is pressed.
+
+        Returns
+        -------
+        bool
+            Whether the button is pressed or not.
+
+        """
+        return not self.io_handler.input(self.BUTTON_RIGHT)
+
+    @property
+    def left(self):
+        """Check if the left button is pressed.
+
+        Returns
+        -------
+        bool
+            Whether the button is pressed or not.
+
+        """
+        return not self.io_handler.input(self.BUTTON_LEFT)
+
+    def get_all(self):
+        """Get all buttons that are pressed.
+
+        Returns
+        -------
+        list of str
+            With all the buttons that where pressed.
+
+        """
+        buttons = []
+
+        if self.up:
+            buttons.append('UP')
+        if self.down:
+            buttons.append('DOWN')
+        if self.right:
+            buttons.append('RIGHT')
+        if self.left:
+            buttons.append('LEFT')
+
+        return buttons
+
+# thumbstick = ThumbStick()
