184f172b7fad44c67fe096bd5f54a7c22fb755f6
Tudor Avram <t.avram@eyeo.com>
2019-07-13 17:54:38 +0200

    added setup function for the display

---
+++ the_pad/demos/button_controllers.py
@@ -0,0 +1,9 @@
+import controller
+
+
+def run():
+    thumbslide, buttons, dpad = controller.configure_directional_controllers()
+
+    while True:
+        pass
+
---
+++ the_pad/display.py
@@ -0,0 +1,55 @@
+from machine import SPI, Pin, I2C
+
+import pins
+import ili934xhax
+
+__all__ = ['setup']
+
+DEFAULT_RES = (240, 320)
+DEFAULT_BACKGROUND = (0, 0, 0)
+
+
+def setup(res=None, background=None, framebuf=False):
+    """Initiate and setup the pad's display.
+
+    Parameters
+    ----------
+    res: tuple
+        Represents the resolution of the screen, as a (width, height) tuple.
+        If `None`, it will use a default value. Default `None`.
+    background: tuple
+        Represents the initial background color of the screen, as a (r, g, b)
+        tuple. If `None`, it will default to (0, 0, 0). Default `None`.
+    framebuf: bool
+        Whether the micropython's framebuf module is in use or not.
+
+    Returns
+    -------
+    ili934xhax.ILI9341
+        With the configured display.
+
+    """
+    if res is None:
+        res = DEFAULT_RES
+
+    if background is None:
+        background = DEFAULT_BACKGROUND
+
+    if framebuf:
+        color_fn = ili934xhax.color565
+    else:
+        color_fn = ili934xhax.color565n
+
+    spi = SPI(2, baudrate=40000000, miso=Pin(pins.SPI_MISO),
+              mosi=Pin(pins.SPI_MOSI), sck=Pin(pins.SPI_CLK))
+
+    display = ili934xhax.ILI9341(spi, cs=Pin(0), dc=Pin(15), rst=Pin(5))
+
+    display.erase()
+    display.set_pos(0, 0)
+    display.width = res[0]
+    display.height = res[1]
+
+    display.fill_rectangle(0, 0, res[0], res[1], color_fn(*background))
+
+    return display
