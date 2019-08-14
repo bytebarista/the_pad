69dfa08276298ccfe8a330185975d6518223a22c
Mischa Diehm <md+gitlab@mailq.de>
2019-08-14 12:50:01 +0200

    be more consistant with the esp8266 codebase

--- APA102.py
+++ APA102.py
@@ -3,26 +3,29 @@ from machine import Pin, SPI
 
 class APA102:
 
-    values = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-    ]
+    #XXX not used yet - do we need it?
+    ORDER = (0, 1, 2, 3)
 
-    def __init__(self, spi, n):
-        super().__init__()
+    leds = []
+
+    def __init__(self, spi, n=4):
         self.spi = spi
         self.spi.init(baudrate=4000000)
         self.n = n
+        # init led value matrix
+        for i in range(n):
+            self.leds.append([0, 0, 0, 0])
 
     def __setitem__(self, index, color):
-        # color is 4d array (first item is brightness)
-        self.values[index] = color
+        # color is 4d array (last item is brightness)
+        # brightness is 5Bit
+        if not (0 <= color[3] <= 31):
+            raise ValueError("Invalid brightness! Values: 0(off) to 31(full)")
+        self.leds[index] = color
         self.write()
 
     def __getitem__(self, index):
-        return self.values[index]
+        return self.leds[index]
 
     def fill(self, color):
         for i in range(self.n):
@@ -38,28 +41,28 @@ class APA102:
         self._send_byte(bytearray([0xFF] * 4))
 
     def reset(self):
-        # TODO: set `values` to zero
+        # TODO: set `leds` to zero
         self._start_frame()
         for i in range(4):
             self._send_byte(bytearray([0xE0] + [0] * 3))
         self._end_frame()
 
     def _send_single(self, color):
-        # color is 4d array (first item is brightness)
-        self._send_byte(bytearray([0xE0 + color[0]] + [color[1]] + [color[2]] + [color[3]]))
+        # default: color is 4d array (last item is brightness)
+        self._send_byte(bytearray([0xE0 + color[3]] + [color[2]] + [color[1]] + [color[0]]))
 
     def write(self):
         self._start_frame()
 
-        for i in range(len(self.values)):
-            self._send_single(self.values[i])
+        for i in range(len(self.leds)):
+            self._send_single(self.leds[i])
 
         self._end_frame()
 
 
 def main():
     led = APA102(SPI(2, sck=Pin(13), mosi=Pin(4), miso=Pin(12)), n=4)
-    led[0] = [255, 255, 0, 0]
-    led[1] = [255, 0, 255, 0]
-    led[2] = [255, 0, 0, 255]
-    led[3] = [255, 255, 255, 0]
+    led[0] = [255, 0, 0, 31]
+    led[1] = [0, 255, 0, 31]
+    led[2] = [0, 0, 255, 31]
+    led[3] = [255, 255, 0, 31]
